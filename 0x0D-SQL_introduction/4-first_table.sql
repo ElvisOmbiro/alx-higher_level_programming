ef05d5304a94224b3d3353fc492eadb721d4089f
ElvisOmbiro <elvisomiro99@gmail.com>
2024-02-13 08:24:09 +0000

    4-first_table.sql

---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,2 @@
+-- a script that creates table in database currently in mysql server
+CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256))
---
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -0,0 +1,2 @@
+-- a script that lists all rows of first_table in hbtn_0c_0 in mysql server
+SELECT * FROM first_table
