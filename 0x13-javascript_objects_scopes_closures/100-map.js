f1220f8bec48dd2ac5a6dd701e41cdaf80810081
ElvisOmbiro <elvisombiro99@gmail.com>
2024-03-13 10:16:29 +0300

    100-map.js

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,5 +1,9 @@
 #!/usr/bin/node
 
-const list = require('./100-data.js').list;
-console.log(list);
-console.log(list.map((item, index) => item * index));
+const list = require('./100-data');
+
+console.log('Initial list:', list);
+
+const listTwo = list.map((value, index) => value * index);
+
+console.log('New list:', listTwo);
