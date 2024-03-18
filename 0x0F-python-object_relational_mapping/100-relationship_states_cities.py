dba6c4e17e4e2a2edb9aaeaaf62d2ed0b2d6ac6f
ElvisOmbiro <elvisombiro99@gmail.com>
2024-03-18 14:35:39 +0300

    100-relationship_states_cities.py

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -1,59 +1,32 @@
-!/usr/bin/python3
+#!/usr/bin/python3
 """
-Adds the State object “California” with the City “San Francisco”
-to the database hbtn_0e_100_usa
+This script prints all City objects
+from the database `hbtn_0e_14_usa`.
 """
 
-import sys
+from sys import argv
+from relationship_state import Base, State
+from relationship_city import City
 from sqlalchemy import create_engine
-from relationship_state import State
-from relationship_city import Base, City
 from sqlalchemy.orm import sessionmaker
-
-
 
 if __name__ == "__main__":
     """
-    Get MySQL credentials
+    Access to the database and get the cities
+    from the database.
     """
-    mysql_username = sys.argv[1]
-    mysql_password = sys.argv[2]
-    database_name = sys.argv[3]
 
-    """
-    Create engine and link to database
-    """
-    engine = create_engine(
-        'mysql+mysqldb://{}:{}@localhost:3306/{}'
-        .format(mysql_username, mysql_password, database_name),
-        pool_pre_ping=True)
-
-    """
-    Create all tables in the engine
-    """
+    db_uri = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        argv[1], argv[2], argv[3])
+    engine = create_engine(db_uri)
     Base.metadata.create_all(engine)
-
-    """
-    Create session
-    """
     Session = sessionmaker(bind=engine)
+
     session = Session()
+    cal_state = State(name='California')
+    sfr_city = City(name='San Francisco')
+    cal_state.cities.append(sfr_city)
 
-    """
-    Create California State object
-    """
-    california = State(name="California")
-
-    """
-    Create San Francisco City object and link it to California
-    """
-    san_francisco = City(name="San Francisco")
-    california.cities.append(san_francisco)
-
-    """
-    Add California and San Francisco to session
-    and commit changes
-    """
-    session.add(california)
-    session.add(san_francisco)
+    session.add(cal_state)
     session.commit()
+    session.close()
