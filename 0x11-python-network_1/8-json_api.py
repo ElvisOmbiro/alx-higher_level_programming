98b0254d355f6cfa5322bd60ad9d65138ab1a500
ElvisOmbiro <elvisombiro99@gmail.com>
2024-03-29 00:24:47 +0300

    8-json_api.py

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,24 +1,32 @@
 #!/usr/bin/python3
-"""Sends a request to the URL and displays the body of the response."""
-
+"""
+script that takes in a letter and sends a POST request to
+http://0.0.0.0:5000/search_user with the letter as a
+parameter.
+"""
+import requests
+import sys
 
 if __name__ == '__main__':
-    from requests import post
-    from sys import argv
-
-    URL = 'http://0.0.0.0:5000/search_user'
-    data = {'q': argv[1] if len(argv) >= 2 else ""}
-    response = post(URL, data)
-
-    type_res = response.headers['content-type']
-
-    if type_res == 'application/json':
-        result = response.json()
-        _id = result.get('id')
-        name = result.get('name')
-        if (result != {} and _id and name):
-            print("[{}] {}".format(_id, name))
-        else:
-            print('No result')
+    if len(sys.argv) == 2:
+        q = sys.argv[1]
     else:
-        print('Not a valid JSON')
+        q = ""
+
+    url = 'http://0.0.0.0:5000/search_user'
+    data = {'q': q}
+
+    r = requests.post(url, data=data)
+
+    try:
+        json_response = r.json()
+    except ValueError:
+        print("Not a valid JSON")
+        sys.exit()
+
+    if json_response:
+        print("[{}] {}".format(
+            json_response.get("id"), json_response.get(
+                "name")))
+    else:
+        print("No result")
