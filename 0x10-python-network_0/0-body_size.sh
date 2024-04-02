1d52daf416c6ed334b30db23e817dfe570d3cab0
ElvisOmbiro <elvisomiro99@gmail.com>
2024-04-02 10:20:20 +0000

    4-header.sh

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#a Bash script that takes in a URL, sends a GET request to the URL, and displays the body of the response
-curl -sL "$1"
+# send a request to an URL with curl, and displays the size of the body of the response
+curl -s "$1" | wc -c
